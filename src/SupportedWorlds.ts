f7d51520efae994364ae2594236336f4d69d2d87
Brennan Fife <brenfife@gmail.com>
2022-02-09 12:02:24 -0700

    chore: switch nft worlds analytics to true

--- src/SupportedWorlds.ts
+++ src/SupportedWorlds.ts
@@ -202,7 +202,7 @@ export const SupportedWorlds: ISupportedWorlds[] = [
     exists: {
       map: false,
       leaderboard: false,
-      analytics: false,
+      analytics: true,
     },
   },
 ];
