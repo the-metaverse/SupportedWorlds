e217e28c6cff93a9ff888e01f1cfb02cba292308
Brennan Fife <brenfife@gmail.com>
2022-02-09 15:20:15 -0700

    chore: add prettier, lint

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,26 @@
-import express, { Request, Response } from "express";
-import { SupportedWorlds } from "./SupportedWorlds";
-import cors from "cors";
+import cors from 'cors';
+import express, { Request, Response } from 'express';
+
+import { SupportedWorlds } from './SupportedWorlds';
 
 const app = express();
 
 app.use(
   cors({
     origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://wemeta-analytics-client-dev.herokuapp.com/welcome",
-      "https://analytics.wemeta.world/welcome",
-      "https://wemeta-analytics-client-dev.herokuapp.com",
-      "https://analytics.wemeta.world",
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://wemeta-analytics-client-dev.herokuapp.com/welcome',
+      'https://analytics.wemeta.world/welcome',
+      'https://wemeta-analytics-client-dev.herokuapp.com',
+      'https://analytics.wemeta.world',
     ],
-  })
+  }),
 );
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send(SupportedWorlds);
 });
 
