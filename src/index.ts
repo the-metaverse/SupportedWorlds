3ed3199dbb651b3eee24c4f23409a40fa2c0bfd4
Brennan Fife <brenfife@gmail.com>
2022-02-08 17:14:45 -0700

    chore: cors cont

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-import express, { Application, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import { SupportedWorlds } from "./SupportedWorlds";
 import cors from "cors";
 
-const app: Application = express();
+const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://wemeta-supported-worlds-prod.herokuapp.com/",
+      "https://wemeta-supported-worlds-dev.herokuapp.com/",
+    ],
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send(SupportedWorlds);
 });
 
-app.listen(process.env.PORT || 5000, function () {
-  console.log(`App is running!`);
-});
+app.listen(process.env.PORT || 5000);
