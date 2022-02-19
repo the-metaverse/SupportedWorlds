f095a1cc1b6ed6d8de288c0f1ac777f5ab520af2
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-02-18 19:05:24 -0500

    refactor: moved json data into a data dir

--- src/SupportedCommunities.ts
+++ src/SupportedCommunities.ts
@@ -1,4 +1,4 @@
-import communities from '../communities.json';
+import communities from '../data/communities.json';
 
 export const SupportedCommunities: string[] = JSON.parse(
   JSON.stringify(communities),
